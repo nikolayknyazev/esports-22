656c2342b13ae46f8b1c82b968baaaa0fbeb97b3
Egor Bazakin <egor.bazakin.09@gmail.com>
2026-07-15 13:02:31 +0300

    Добавление исходного проекта задания JS

---
+++ submissions/06-js/frontend/src/pages/main/Main.tsx
@@ -0,0 +1,11 @@
+import './Main.css';
+
+export const MainPage: React.FC = () => {
+    return (
+        <div className="main-page">
+            <div className="title-with-cat">
+                Задание для студентов
+            </div>
+        </div>
+    );
+};
