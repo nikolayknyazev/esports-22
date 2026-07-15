4ffb7b0f25d98ce424307be56288297dbe30dc84
Egor Bazakin <egor.bazakin.09@gmail.com>
2026-07-15 15:48:14 +0300

    feat: initial sections main page

--- submissions/06-js/frontend/src/pages/main/Main.tsx
+++ submissions/06-js/frontend/src/pages/main/Main.tsx
@@ -3,10 +3,34 @@ import './Main.css';
 import { ThemeSwitcher } from '../../components/ThemeSwitcher';
 
 export const MainPage: React.FC = () => {
-    return (
-        <div className="main-page">
-            Задание для студентов
-            <ThemeSwitcher />
-        </div>
-    );
+  return (
+    <div className="main-page">
+      Задание для студентов
+      <ThemeSwitcher />
+
+      <section className="section-now">
+        now
+      </section>
+
+      <section className="section-games">
+        games
+      </section>
+
+      <section className="section-today">
+        today
+      </section>
+
+      <section className="section-news">
+        news
+      </section>
+
+      <section className="section-tops">
+
+      </section>
+
+      <section className="section-sub">
+        subscribe
+      </section>
+    </div>
+  );
 };
