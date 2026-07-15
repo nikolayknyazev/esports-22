e424bba111171e22a27049ed7c06f5dcb774f872
Egor Bazakin <egor.bazakin.09@gmail.com>
2026-07-15 14:31:44 +0300

    feat: add themeContext created themeSwitcher

--- submissions/06-js/frontend/src/pages/main/Main.tsx
+++ submissions/06-js/frontend/src/pages/main/Main.tsx
@@ -1,10 +1,13 @@
 import './Main.css';
 
+import { ThemeSwitcher } from '../../components/ThemeSwitcher';
+
 export const MainPage: React.FC = () => {
     return (
         <div className="main-page">
             <div className="title-with-cat">
                 Задание для студентов
+                <ThemeSwitcher />
             </div>
         </div>
     );
