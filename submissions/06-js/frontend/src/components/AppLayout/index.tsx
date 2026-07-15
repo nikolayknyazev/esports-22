21db157e0159a91b00bd01bdc572c1dda304a607
Egor Bazakin <egor.bazakin.09@gmail.com>
2026-07-15 13:48:14 +0300

    refactor: removed unused Sider and fix linter errors

--- submissions/06-js/frontend/src/components/AppLayout/index.tsx
+++ submissions/06-js/frontend/src/components/AppLayout/index.tsx
@@ -1,5 +1,5 @@
 import {Layout, Typography} from 'antd';
-import Sider from 'antd/es/layout/Sider';
+// import Sider from 'antd/es/layout/Sider';
 import {Navigate, Route, Routes} from 'react-router';
 
 import {MainPage} from '../../pages/main/Main';
@@ -9,19 +9,19 @@ import {TeamsPage} from '../../pages/teams';
 import {TeamCardPage} from '../../pages/teams/teamCard';
 import {TournamentInfoPage} from '../../pages/tournamentInfo';
 import {TournamentsPage} from '../../pages/tournaments';
-import {AppMenu} from '../AppMenu';
+// import {AppMenu} from '../AppMenu';
 
 export const AppLayout = () => {
     return (
         <Layout style={{minHeight: '100vh'}}>
-            <Sider width={250} style={{background: '#462b4c'}}>
+            {/* <Sider width={250} style={{background: '#462b4c'}}>
                 <div style={{padding: '24px', color: '#ffffff'}}>
                     <Typography.Title level={4} style={{color: '#ffffff', margin: 0}}>
                         CyberSportsPortal
                     </Typography.Title>
                 </div>
                 <AppMenu/>
-            </Sider>
+            </Sider> */}
             <Layout>
                 <Layout.Content>
                     <Routes>
